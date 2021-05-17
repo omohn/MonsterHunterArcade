1f2f2a012190bc1142b42d9edd5af6fe2729094e
mkorovay <Maryna.Kor@gmail.com>
2021-05-17 14:28:09 +0200

    PlayField Commit ohne Pixelfy

--- src/main/java/org/beta/App.java
+++ src/main/java/org/beta/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,12 +21,13 @@ public class App extends Application {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
+        Font font = Font.loadFont("file:resources/org/beta/fonts/Pixelfy-ow9yd.ttf", 15);
     }
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
-
+    Font font = Font.loadFont("file:resources/org.beta/fonts/TenaliRamakrishna-Regular.ttf", 45);
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
