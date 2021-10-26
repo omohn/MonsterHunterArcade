b5462f7a7e2ecef66f68d16bf38ccaf7778f1a5c
Oliver Mohn <s57998@beuth-hochschule.de>
2021-10-26 12:12:47 +0200

    changed the codes for building walls

--- src/main/java/org/beta/game/PlayField.java
+++ src/main/java/org/beta/game/PlayField.java
@@ -14,13 +14,13 @@ public class PlayField extends Pane {
     private final int WALL_DEPTH = 4;
 
     private int[][] map = {
-            {0, 18, 10, 6, 18, 10, 2, 10, 6, 18, 10, 6, 0},
-            {0, 20, 18, 0, 8, 10, 0, 10, 8, 0, 6, 20, 0},
-            {10, 0, 4, 16, 10, 6, 20, 18, 10, 4, 16, 0, 10},
-            {0, 16, 12, 20, 18, 8, 0, 8, 6, 20, 24, 4, 0},
-            {0, 20, 18, 8, 0, 6, 20, 18, 0, 8, 6, 20, 0},
-            {0, 16, 8, 10, 12, 24, 8, 12, 24, 10, 8, 4, 0},
-            {0, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 28, 0}
+            {0, 9, 5, 3, 9, 5, 1, 5, 3, 9, 5, 3, 0},
+            {0, 10, 9, 0, 4, 5, 0, 5, 4, 0, 3, 10, 0},
+            {5, 0, 2, 8, 5, 3, 10, 9, 5, 2, 8, 0, 5},
+            {0, 8, 6, 10, 9, 4, 0, 4, 3, 10, 12, 2, 0},
+            {0, 10, 9, 4, 0, 3, 10, 9, 0, 4, 3, 10, 0},
+            {0, 8, 4, 5, 6, 12, 4, 6, 12, 5, 4, 2, 0},
+            {0, 14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 0}
     };
 
     private int playFieldRows = map.length;
@@ -32,21 +32,21 @@ public class PlayField extends Pane {
         for (int i = 0; i < playFieldRows; i++) {
             for (int j = 0; j < playFieldCols; j++) {
                 int cell = map[i][j];
-                if (cell - 16 >= 0) {
-                    environment.add(new Wall(j * FIELD_SIZE, i * FIELD_SIZE, false));
-                    cell -= 16;
-                }
                 if (cell - 8 >= 0) {
-                    environment.add(new Wall(j * FIELD_SIZE, i * FIELD_SIZE + FIELD_SIZE - WALL_DEPTH, true));
+                    environment.add(new Wall(j * FIELD_SIZE, i * FIELD_SIZE, false));
                     cell -= 8;
                 }
                 if (cell - 4 >= 0) {
-                    environment.add(new Wall(j * FIELD_SIZE + FIELD_SIZE - WALL_DEPTH, i * FIELD_SIZE, false));
+                    environment.add(new Wall(j * FIELD_SIZE, i * FIELD_SIZE + FIELD_SIZE - WALL_DEPTH, true));
                     cell -= 4;
                 }
                 if (cell - 2 >= 0) {
-                    environment.add(new Wall(j * FIELD_SIZE, i * FIELD_SIZE, true));
+                    environment.add(new Wall(j * FIELD_SIZE + FIELD_SIZE - WALL_DEPTH, i * FIELD_SIZE, false));
                     cell -= 2;
+                }
+                if (cell - 1 >= 0) {
+                    environment.add(new Wall(j * FIELD_SIZE, i * FIELD_SIZE, true));
+                    cell -= 1;
                 }
                 if (cell != 0) {
                     System.out.println("Something's wrong with your Array");
