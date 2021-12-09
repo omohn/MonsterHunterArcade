19e4f6a6f83d2b1a2091bf414282b9809778ca92
Oliver Mohn <s57998@beuth-hochschule.de>
2021-12-09 11:20:19 +0100

    Bug im Leveldesign entfernt, neue Spielfeldcodierung angepasst, siehe MHLeveldesign.pdf

--- src/main/java/org/beta/game/PlayField.java
+++ src/main/java/org/beta/game/PlayField.java
@@ -19,13 +19,13 @@ public class PlayField extends Pane {
     private final int SPRITE_SIZE = FIELD_SIZE - 2 * WALL_DEPTH;
 
     private int[][] map = {
-            {0, 9, 5, 3, 9, 5, 1, 5, 3, 9, 5, 3, 0},
-            {0, 10, 9, 0, 4, 5, 0, 5, 4, 0, 3, 10, 0},
-            {5, 0, 2, 8, 5, 3, 10, 9, 5, 2, 8, 0, 5},
-            {0, 8, 6, 10, 9, 4, 0, 4, 3, 10, 12, 2, 0},
-            {0, 10, 9, 4, 0, 3, 10, 9, 0, 4, 3, 10, 0},
-            {0, 8, 4, 5, 6, 12, 4, 6, 12, 5, 4, 2, 0},
-            {0, 14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 0}
+            {4, 18, 10, 6, 18, 10, 2, 10, 6, 18, 10, 6, 16},
+            {12, 20, 18, 0, 8, 10, 0, 10, 8, 0 , 6, 20, 24},
+            {10,0, 4, 16, 10, 6, 20, 18, 10, 4, 16, 0, 10},
+            {6, 16, 12, 20, 18, 8, 0, 8, 6, 20, 24, 4, 18},
+            {4, 20, 18, 8, 0, 6, 20, 18, 0, 8, 6, 20, 16},
+            {4, 16, 8, 10, 12, 24, 8, 12, 24, 10, 8, 4, 16},
+            {4, 28, 18, 2, 2, 2, 2, 2, 2, 2, 6, 28, 16}
     };
 
     private int playFieldRows = map.length;
@@ -42,21 +42,21 @@ public class PlayField extends Pane {
         for (int i = 0; i < playFieldRows; i++) {
             for (int j = 0; j < playFieldCols; j++) {
                 int cell = map[i][j];
-                if (cell - 8 >= 0) {
+                if (cell - 16 >= 0) {
                     environment.add(new Wall(j * FIELD_SIZE, i * FIELD_SIZE, false));
+                    cell -= 16;
+                }
+                if (cell - 8 >= 0) {
+                    environment.add(new Wall(j * FIELD_SIZE, i * FIELD_SIZE + FIELD_SIZE - WALL_DEPTH, true));
                     cell -= 8;
                 }
                 if (cell - 4 >= 0) {
-                    environment.add(new Wall(j * FIELD_SIZE, i * FIELD_SIZE + FIELD_SIZE - WALL_DEPTH, true));
+                    environment.add(new Wall(j * FIELD_SIZE + FIELD_SIZE - WALL_DEPTH, i * FIELD_SIZE, false));
                     cell -= 4;
                 }
                 if (cell - 2 >= 0) {
-                    environment.add(new Wall(j * FIELD_SIZE + FIELD_SIZE - WALL_DEPTH, i * FIELD_SIZE, false));
-                    cell -= 2;
-                }
-                if (cell - 1 >= 0) {
                     environment.add(new Wall(j * FIELD_SIZE, i * FIELD_SIZE, true));
-                    cell -= 1;
+                    cell -= 2;
                 }
                 if (cell != 0) {
                     System.out.println("Something's wrong with your Array");
